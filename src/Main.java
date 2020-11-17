0e94d6a809f1b1695752fddf014601b446fe9068
Olga Nevezina <nevezhinao@yandex.ru>
2020-11-17 20:19:19 +0300

    Added .gitignore and aviabonus

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 278078;
+        int one_mile = 20;
+        int miles = amount / one_mile;
+        System.out.println(miles);
+    }
+}
